a51049cd69ce6d978adfa986e63733a7e80756a4
Mayokun <mayokun@Mayokuns-MacBook-Air.local>
2026-01-06 22:22:58 +0100

    db

--- backend_prj/settings.py
+++ backend_prj/settings.py
@@ -114,11 +114,17 @@ WSGI_APPLICATION = 'backend_prj.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "CONN_MAX_AGE": 60,  # 👈 THIS IS KEY
     }
 }
+
 
 
 # Password validation
