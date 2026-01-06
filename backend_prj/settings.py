6f83a2925a127711272cf4b0d83bdc3dae83158b
Mayokun <mayokun@Mayokuns-MacBook-Air.local>
2026-01-06 19:42:05 +0100

    cors origin

--- backend_prj/settings.py
+++ backend_prj/settings.py
@@ -216,10 +216,8 @@ SPECTACULAR_SETTINGS = {
 
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173",
+    "https://chophive-frontend-v1.vercel.app",
+    "https://chophive-v1-1.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
